04944583a522fb063d968795b84b85d10edd4cb7
tauheed0110 <tauheed0110@gmail.com>
2024-11-04 13:12:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,30 +1,41 @@
-//complete this code
-class Animal{
-    constructor(species){
+class Animal {
+    constructor(species) {
         this.species = species;
     }
-    makeSound(){
-        console.log(`The ${species} makes a sound`);
+
+    makeSound() { // Changed to a method
+        console.log("The", this.species, "makes a sound");
     }
 }
 
-class Cat extends Animal{
-    constructor(species){
+class Cat extends Animal {
+    constructor(species) {
         super(species);
     }
-    purr(){
+
+    purr() {
         console.log("purr");
     }
 }
 
-class Dog extends Animal{
-    constructor(species){
+class Dog extends Animal {
+    constructor(species) {
         super(species);
     }
-    bark(){
-        console.log("woof")
+
+    bark() {
+        console.log("woof");
     }
 }
+
+// Example usage:
+const myCat = new Cat("Siamese");
+myCat.makeSound(); // Output: The Siamese makes a sound
+myCat.purr(); // Output: purr
+
+const myDog = new Dog("Golden Retriever");
+myDog.makeSound(); // Output: The Golden Retriever makes a sound
+myDog.bark(); // Output: woof
 
 
 // Do not change the code below this line

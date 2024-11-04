3e4a11280c8e0d7b0ecf13a97ca5cee5ec6f96b3
tauheed0110 <tauheed0110@gmail.com>
2024-11-04 13:00:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ class Animal{
     constructor(species){
         this.species = species;
     }
-    makeSound(){
+    get makeSound(){
         console.log("The", this.species,"makes a sound");
     }
 }
